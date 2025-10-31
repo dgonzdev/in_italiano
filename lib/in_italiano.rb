6fef5c7bab41b6a232e3c48f08086db976995359
dgonzdev <dgonzdev837@gmail.com>
2025-10-31 19:03:35 -0400

    add translation for string capitalize method

--- lib/in_italiano.rb
+++ lib/in_italiano.rb
@@ -1,2 +1,12 @@
+require 'in_italiano/string_translations'
+
 module InItaliano
+  @@last_class = nil
+  @@last_method = nil
+
+  TRANSLATIONS = {
+    string: {
+      capitalize: "capitalizzare"
+    }
+  }
 end
---
+++ lib/in_italiano/string_translations.rb
@@ -0,0 +1,21 @@
+module InItaliano
+  module StringTranslations
+    def capitalize
+      @@last_class = :string
+      @@last_method = :capitalize
+
+      super
+    end
+    
+    def method_missing(symbol, *args)
+      if symbol === :in_italiano
+        translation = TRANSLATIONS[@@last_class][@@last_method]
+        @@last_class = nil
+        @@last_method = nil
+        translation
+      else
+        super(symbol, *args)
+      end
+    end
+  end
+end
