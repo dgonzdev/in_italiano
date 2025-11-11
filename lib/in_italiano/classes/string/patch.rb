4072ccbba016875851eec25ccac10df4aeae09b8
dgonzdev <dgonzdev837@gmail.com>
2025-11-11 12:08:56 +0000

    add more translations for string class instance methods

--- lib/in_italiano/classes/string/patch.rb
+++ lib/in_italiano/classes/string/patch.rb
@@ -2,6 +2,17 @@ module InItaliano
   module Classes
     module String
       module Patch
+        # String Class Documentation:
+        # https://docs.ruby-lang.org/en/3.4/String.html
+
+        # Instance Methods
+        def append_as_bytes(*objects)
+          @@last_class = :string
+          @@last_method = :append_as_bytes
+
+          super
+        end
+
         def capitalize
           @@last_class = :string
           @@last_method = :capitalize
